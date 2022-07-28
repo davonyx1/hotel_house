4cea56d1ebeec4b4e123624d6127ce7a076cb07e
davonyx1 <104191705+davonyx1@users.noreply.github.com>
2022-07-28 14:49:35 +0200

    Revert of Revert "controller commande chambre slider"

--- src/Controller/SliderController.php
+++ src/Controller/SliderController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Slider;
-use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,12 +13,10 @@ class SliderController extends AbstractController
     /**
      * @Route("/slider", name="show_slider", methods={"GET|POST"})
      */
-    public function showSlider(EntityManagerInterface $entityManager): Response
+    public function showSlider(Slider $slider): Response
     {
-        $sliders = $entityManager->getRepository(SLider::class)->findAll();
-
         return $this->render('slider/show_slider.html.twig', [
-            'sliders' => $sliders
+            'slider' => $slider
         ]);
     }
 }
