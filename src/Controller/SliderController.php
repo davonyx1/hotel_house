03653c94065443fc556def429330faf13faae5d2
davonyx1 <104191705+davonyx1@users.noreply.github.com>
2022-07-28 15:08:40 +0200

    Reset slider

--- src/Controller/SliderController.php
+++ src/Controller/SliderController.php
@@ -2,19 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Slider;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SliderController extends AbstractController
 {
     /**
-     * @Route("/slider", name="app_slider")
+     * @Route("/slider", name="show_slider", methods={"GET|POST"})
      */
-    public function index(): Response
+    public function showSlider(Slider $slider): Response
     {
-        return $this->render('slider/index.html.twig', [
-            'controller_name' => 'SliderController',
+        return $this->render('slider/show_slider.html.twig', [
+            'slider' => $slider
         ]);
     }
 }
